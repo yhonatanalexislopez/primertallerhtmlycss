034db7b2284ceb0badf752ea76b9e6040972871f
yhonatan alexis lopez <yhonatanalexislopezsalazar@gmail.com>
2020-10-28 14:20:34 -0500

    se le camvia la letra

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            primertallerhtmlycss
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    </head>
+    <body>
+
+        <!--mi titulo-->
+        <center><h1>la naturalesa</h1></center>
+        <p align=center><font size="5" color="white" face="Copperplate">
+            La naturaleza, en su sentido más amplio, es equivalente al mundo natural, mundo material o universo material.
+             
+        </font></p>
+        <center><img id="salvage1" src="images/naturalesacolombiana.jpg" width="700" height="500"></center>
+        <center><h1>paisajes</h1></center>
+        <p align=center><font size="5" color="white" face="Copperplate">
+            este es un paisaje hermoso que comtelpla lo manifico de la </br>naturalesa con un bosque esuberante con una cascada que da a </br>enternder que aun es virgen </br>
+        </font></p>
+        <center><img id="paisajes1" src="images/cascada.jpg"
+        width="500" height="250"></center>
+
+    </body>
+</html>
